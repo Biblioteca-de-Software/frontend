bf55cd6be000dedc5e6bb90ddaff6e5b315c205c
Ayrton <u202311077@upc.edu.pe>
2025-05-12 21:35:08 -0500

    feat(report): add report response.

---
+++ src/app/report/services/report.response.ts
@@ -0,0 +1,14 @@
+export interface ReportResponse {
+  items: ReportResource[];
+}
+
+export interface ReportResource {
+
+  id: string;                // Identificador único del reporte
+  title: string;             // Título del reporte
+  details: string;           // Detalles adicionales o análisis
+  type: string;              // Tipo de reporte: 'Ingreso', 'Salida', 'Inventario', 'Alerta', etc.
+  date: string;              // Fecha de creación del reporte
+  status: string;            // Estado del reporte: 'Pendiente', 'Revisado', 'Aprobado'
+
+}
