088b4953d7d715df49ac4dcd4e48db155b6f75a2
Werner Lang <u202310003@upc.edu.pe>
2025-06-18 19:36:32 -0500

    feat(report): add getReports and addReports

--- src/app/reports/services/report.service.ts
+++ src/app/reports/services/report.service.ts
@@ -1,22 +1,42 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {map, Observable, retry} from 'rxjs';
 
 import { ReportEntity } from '../model/report-entity';
 import { environment} from '../../../environments/environment';
+import {BaseService} from '../../shared/services/base.service';
+
+const orderResourceEndpoint = environment.reportEndpointPath;
 
 
 @Injectable({ providedIn: 'root' })
-export class reportService {
-  private apiUrl = 'http://localhost:3000/api/v1/reports';
+export class reportService extends BaseService<ReportEntity> {
 
-  constructor(private http: HttpClient) {}
+  constructor() {
+    super();
+    this.resourceEndpoint = orderResourceEndpoint;
+  }
+
 
   getReports(): Observable<ReportEntity[]> {
-    return this.http.get<ReportEntity[]>(`${environment.serverBaseUrl}${environment.reportProviderApiBaseUrl}`);
+    return this.http.get<Array<any>>(this.resourcePath(), this.httpOptions).pipe(
+      retry(2),
+      map((rReports: any[]) => rReports.map(r => new ReportEntity(r.id, r.title, r.description)))
+    );
   }
 
-  addReports(report: ReportEntity): Observable<any> {
-    return this.http.post<any>(`${environment.serverBaseUrl}${environment.reportProviderApiBaseUrl}`, report);
+  addReports(payload:{
+    title: string;
+    details: string;
+  }): Observable<ReportEntity>
+  {
+    return this.http.post<ReportEntity>(this.resourcePath(), payload, this.httpOptions)
   }
+
+  //getReports(): Observable<ReportEntity[]> {
+  //  return this.http.get<ReportEntity[]>(`${environment.serverBaseUrl}${environment.reportProviderApiBaseUrl}`);
+  //}
+
+  //addReports(report: ReportEntity): Observable<any> {
+    //return this.http.post<any>(`${environment.serverBaseUrl}${environment.reportProviderApiBaseUrl}`, report);
+  //}
 }
