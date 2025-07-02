57406915c0a02f8be77fd6157a65ade8a8dc4436
Werner Lang <U202310003@upc.edu.pe>
2025-07-02 12:39:46 -0500

    fix: Sends params incorrectly

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  serverBaseUrl: 'http://localhost:3306/api/v1',
+  serverBaseUrl: 'http://localhost:8080/api/v1',
   reportProviderApiBaseUrl: '/reports',
   reportEndpointPath: '/reports',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBaseUrl: 'http://localhost:3306/api/v1',
+  serverBaseUrl: 'http://localhost:8080/api/v1',
   reportEndpointPath: '/reports',
 };
